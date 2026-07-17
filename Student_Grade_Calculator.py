3ea4e812c61babbb3e29b0e3a871ade0e68d2c11
Raghavendra-lucifer-118 <raghavendrav2275@gmail.com>
2026-07-17 19:07:09 +0530

    Added Exception handling and Name-main Idiom

--- Student_Grade_Calculator.py
+++ Student_Grade_Calculator.py
@@ -1,17 +1,35 @@
-#Input for name and Roll Number
-name = input("Enter Name: ")
-rollNumber = int(input("Enter Roll Number: "))
+def calculate_grade(name , rollNumber, marks):
+     # Output Grades
+        print("Name: ", name)    
+        print("Roll Number: ", rollNumber)
+        print("Total Marks : ", marks)
+        print("Average: ", round(marks/3 ,3),"%")
+        print("pass" if (marks/3) > 40 else "Fail")
 
-marks = 0
-for _ in range(3):
-    mark = int(input(f"Enter Subject {_+1} marks: "))
-    marks += mark
+
+def main():
+    #Input for name and Roll Number
+    name = input("Enter Name: ")
+    rollNumber = int(input("Enter Roll Number: "))
+
+    marks = 0
+
+    try:
+        # Taking input of marks for the subjects
+        for _ in range(3):
+            mark = int(input(f"Enter Subject {_+1} marks: "))
+            marks += mark
+        
+        calculate_grade(name,rollNumber , marks)    
+        
+       
+    except Exception as err:
+        print(f"Error is: {err}")
+        print("Cannot genrate for this data")     
     
-
-# Output Grades
-print("Name: ", name)    
-print("Roll Number: ", rollNumber)
-print("Total Marks : ", marks)
-print("Average: ", round(marks/3 ,3),"%")
-print("pass" if (marks/3) > 40 else "Fail")
+    finally:
+        print("Thank You!!!")
+    
+if __name__ == "main":
+    main()
     
