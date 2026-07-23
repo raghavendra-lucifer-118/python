ac76937e60a1c20fb7ad4f34885158ee56a107d2
Raghavendra-lucifer-118 <raghavendrav2275@gmail.com>
2026-07-23 23:18:49 +0530

    Revision name-main idiom and exception handling

--- File_handling_with_CSV.py
+++ File_handling_with_CSV.py
@@ -6,14 +6,27 @@ data = [
     ['Maria', '22', 'Kazan']
 ]
 
-with open("person.csv" , newline="" ,mode="w") as f:
-    writer = csv.writer(f , delimiter=";")
-    writer.writerows(data)
+def main():
+    try:
+        with open("person.csv" , newline="" ,mode="w") as f:
+            writer = csv.writer(f , delimiter=";")
+            writer.writerows(data)
+    except Exception as e:
+        print("The Error is: " , e)    
     
-with open("person.csv" , mode="r") as f:
-    reader = csv.reader(f)
     
-    headers = next(reader)
-    print(f"Headers : {headers}")
-    for row in reader:
-        print(f"{row[0]}")
+    try:
+        with open("person.csv" , mode="r") as f:
+            reader = csv.reader(f)
+    
+            headers = next(reader)
+            print(f"Headers : {headers}")
+            for row in reader:
+                print(f"{row[0]}")
+    
+    except Exception as e:
+        print("The Error is: " , e)    
+
+
+if __name__ == "__main__":
+    main()
