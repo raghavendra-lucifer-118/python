f8f198671d24c649df1fbcb21505e723549d501d
Raghavendra-lucifer-118 <raghavendrav2275@gmail.com>
2026-07-23 23:16:13 +0530

    Revision name-main idiom and exception hnadling

--- File_handling_with_JSON.py
+++ File_handling_with_JSON.py
@@ -6,14 +6,30 @@ person = {
     "languages": ["Python", "JavaScript"]
 }
 
-json_string = json.dumps(person,ensure_ascii=False,indent=2)
-print(json_string)
+def main():
+    json_string = json.dumps(person,ensure_ascii=False,indent=2)
+    print(json_string)
 
-data = json.loads(json_string)
-print(data , type(data))
+    data = json.loads(json_string)
+    print(data , type(data))
 
-with open("person.json" , "w") as f:
-    json.dump(person , f , ensure_ascii=False , indent=2)
+    # Writing JSON to File
+    try:
+        with open("person.json" , "w") as f:
+            json.dump(person , f , ensure_ascii=False , indent=2)
+    except Exception as e:
+        print("The Error is: " , e)   
     
-with open("person.json" , "r") as f:
-     print(json.load(f))
+         
+    # Reading JSON from File
+    try:
+        with open("person.json" , "r") as f:
+            print(json.load(f))
+    except Exception as e:
+        print("The Error is: " , e) 
+
+if __name__ == "__main__" :
+    main()       
+               
+    
+    
